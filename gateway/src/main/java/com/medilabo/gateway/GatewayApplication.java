0c45229b956a87a1c6c2c5abe2abfa4712393a3d
ydiouy <ydiouy@gmail.com>
2025-08-04 15:17:43 +0200

    Gateway: gateway artifact init

--- gateway/src/main/java/com/medilabo/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/medilabo/gateway/GatewayApplication.java
@@ -2,11 +2,22 @@ package com.medilabo.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
-@EnableConfigServer
 @SpringBootApplication
 public class GatewayApplication {
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/get")
+						.filters(f -> f.addRequestHeader("Hello", "World"))
+						.uri("http://httpbin.org:80"))
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
