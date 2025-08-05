c50e8f65d82a4c4421f84faf9870cb659a963813
ydiouy <ydiouy@gmail.com>
2025-08-05 16:54:59 +0200

    Gateway: clean dependencies + disable cloud config + add gateway & eureka config

--- gateway/src/main/java/com/medilabo/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/medilabo/gateway/GatewayApplication.java
@@ -2,20 +2,35 @@ package com.medilabo.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@RestController
+@EnableConfigurationProperties(UriConfiguration.class)
 public class GatewayApplication {
 
 	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+	public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
+		String httpUri = uriConfiguration.getHttpbin();
+
 		return builder.routes()
 				.route(p -> p
-						.path("/get")
-						.filters(f -> f.addRequestHeader("Hello", "World"))
-						.uri("http://httpbin.org:80"))
+					.path("/get")
+					.filters(f -> f.addRequestHeader("Hello", "World"))
+					.uri(httpUri))
+				.route(p -> p
+					.host("*.circuitbreaker.com")
+					.filters(f -> f.circuitBreaker(config -> config
+						.setName("mycmd")
+						.setFallbackUri("forward:/fallback")))
+					.uri(httpUri))
 				.build();
 	}
 
@@ -23,4 +38,23 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	@RequestMapping("/fallback")
+	public Mono<String> fallback() {
+		return Mono.just("fallback");
+	}
+
+}
+
+@ConfigurationProperties
+class UriConfiguration {
+
+	private String httpbin = "http://httpbin.org:80";
+
+	public String getHttpbin() {
+		return httpbin;
+	}
+
+	public void setHttpbin(String httpbin) {
+		this.httpbin = httpbin;
+	}
 }
