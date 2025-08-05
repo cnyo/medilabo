161d496116ed9d17ef80516f519720799e93c34b
ydiouy <ydiouy@gmail.com>
2025-08-05 16:49:12 +0200

    Front: clean code + adapt to gateway service

--- front/src/main/java/com/medilabo/front/controller/FrontController.java
+++ front/src/main/java/com/medilabo/front/controller/FrontController.java
@@ -1,25 +1,41 @@
 package com.medilabo.front.controller;
 
 import com.medilabo.front.beans.PatientBean;
-import com.medilabo.front.proxies.MicroservicePatientsProxy;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 @Controller
 public class FrontController {
-    private final MicroservicePatientsProxy patientsProxy;
+    private final WebClient webClient;
 
-    public FrontController(MicroservicePatientsProxy patientsProxy) {
-        this.patientsProxy = patientsProxy;
+    public FrontController(WebClient.Builder builder) {
+        this.webClient = builder.baseUrl("http://localhost:9001").build();
+    }
+
+    @GetMapping("/hello")
+    public Mono<String> index() {
+        return webClient.get()
+                .uri("/api/hello")
+                .retrieve()
+                .bodyToMono(String.class);
     }
 
     @GetMapping("/")
     public String index(Model model) {
-        List<PatientBean> patients = patientsProxy.getPatients();
+        List<PatientBean> patients = webClient.get()
+                .uri("/api/patients")
+                .retrieve()
+                .bodyToFlux(PatientBean.class)
+                .collectList()
+                .block();
+
         model.addAttribute("patients", patients);
 
         return "index";
@@ -27,17 +43,33 @@ public class FrontController {
 
     @GetMapping("/detail-patient/{id}")
     public String fichePatient(@PathVariable int id, Model model) {
-        PatientBean patient = patientsProxy.getPatient(id);
+        PatientBean patient = webClient.get()
+                .uri("/api/patients/" + id)
+                .retrieve()
+                .bodyToMono(PatientBean.class)
+                .block();
+
         model.addAttribute("patient", patient);
 
         return "fichePatient";
     }
 
     @PostMapping(value="/update-patient/{id}")
-    public String updatePatient(@PathVariable int id, PatientBean updatedPatient) {
-        System.out.println(updatedPatient);
-        ResponseEntity<PatientBean> patient = patientsProxy.updatePatient(id, updatedPatient);
+    public String updatePatient(@PathVariable int id, @ModelAttribute PatientBean updatedPatient) {
+        try {
+            ResponseEntity<PatientBean> response = webClient.put()
+                    .uri("/api/patients/" + id)
+                    .bodyValue(updatedPatient)
+                    .retrieve()
+                    .toEntity(PatientBean.class)
+                    .block();
 
-        return "redirect:/detail-patient/" + id;
+            // Todo: handle response status and errors
+
+            return "redirect:/detail-patient/" + id;
+        } catch (WebClientResponseException e) {
+            // Par exemple : afficher un message d'erreur ou rediriger ailleurs
+            return "error-page";
+        }
     }
 }
