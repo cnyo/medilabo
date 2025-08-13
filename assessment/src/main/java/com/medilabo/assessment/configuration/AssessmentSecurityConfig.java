a094b7f20080e541d2438b488c92a90c990f4875
ydiouy <ydiouy@gmail.com>
2025-08-13 20:48:51 +0200

    Assessment: init service

---
+++ assessment/src/main/java/com/medilabo/assessment/configuration/AssessmentSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.medilabo.assessment.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+public class AssessmentSecurityConfig {
+    @Bean
+    public UserDetailsService users() {
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+
+        userDetailsService.createUser(User
+                .withUsername("user")
+                .password("{noop}user")   // {noop} pour tests : mot de passe en clair
+                .roles("GATEWAY")
+                .build());
+
+        return userDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                                .anyRequest().permitAll()
+//                        .anyRequest().authenticated()
+//                        .anyRequest().access(hasIpAddress("127.0.0.1"))
+                )
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
+}
